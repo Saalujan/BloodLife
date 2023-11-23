99ac5aa96030e0f13ef9c6760d620c0d68ef373c
Sasikumar Saalujan <126445229+Saalujan@users.noreply.github.com>
2023-11-23 12:41:08 +0530

    some changes

--- login.php
+++ login.php
@@ -1,18 +1,74 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
-    <title>Login Page in HTML with CSS</title>
-    <body>
-    <div class="card">
-  <img src="../BloodLife/Images/rm209-ning-20.jpg"; alt="Background Picture">
-  <div class="card-content">
-    <!-- Your content goes here -->
-    <h2>Card Title</h2>
-    <p>Card description or text</p>
+  <meta charset="UTF-8">
+  <title></title>
+
+<body>
+  <div class="row">
+    <div class="col-4">
+      <div class="row bg-white m-2 pt-0  align-items-center justify-content-center rounded-5" style="height: 400px; width:400px;box-shadow: rgba(0, 0, 0, 0.24) 2px 3px 8px;">
+      </div>
+    </div>
+    <div class="col-4">
+      <div class="row bg-white m-2 pt-0  align-items-center justify-content-center rounded-5" style="height: 400px; width:400px;box-shadow: rgba(0, 0, 0, 0.24) 2px 3px 8px;">
+
+      </div>
+
+    </div>
+  </div> -->
+<!-- <div class="row">
+    <div class="col-5">
+      <div class="col-3 rounded-4 m-3 " style="width: 500px; height: 700px;box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px; border-radius:10px; margin-left:50px;">
+
+        
+      </div>
+    </div>
+
+    <div class="col-5">
+      <div class="col-3 rounded-4 m-3 " style="width: 500px; height: 700px;box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px; border-radius:10px; margin-left:50px;">
+
+        <div class="row">
+          <div class="col-1 rounded-right-0" style="width: 10px; height:160px; border-top-left-radius: 10px; border-bottom-left-radius: 10px;">
+
+          </div>
+          <div class="col-10 bg-white p-2 pt-3 mb-1" style="width:240px">
+
+
+            <div class="row">
+
+              <div class="row">
+                <div class="col-5" style="height: 40px;">
+
+
+                </div>
+                <div class="col-5 text-end" style="height: 40px;">
+
+                </div>
+              </div>
+
+              <div class="row" style="height: 30px;">
+
+
+              </div>
+              <div class="row">
+
+              </div>
+              <div class="row pt-3" style="height: 25px;">
+
+
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
   </div>
-</div>
-    </body>
+
+</body>
 
 
 
@@ -20,4 +76,86 @@
 
 
 </head>
+
+</html> -->
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Sign In</title>
+  <style>
+    /* Basic styling for the sign-in form */
+    
+
+    input[type="text"],
+    input[type="password"],
+    input[type="submit"] {
+      width: 100%;
+      padding: 10px;
+      margin: 8px 0;
+      display: inline-block;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      box-sizing: border-box;
+    }
+
+    input[type="submit"] {
+      background-color: #000000;
+      color: white;
+      border: none;
+      cursor: pointer;
+    }
+
+    
+  </style>
+</head>
+
+<body>
+  <div class="row">
+   
+          <div class="col-lg-6">
+            <div class="form-container" style=" margin-left:100px;height: 400px;width: 400px; background: #fff; padding: 20px;border-radius: 5px; box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.1);">
+              <div class="card-body">
+                <h2 class="row" style="font-size: 40px; color:red">
+                  <center>Hospital</center>
+                </h2>
+                <form>
+                  <label for="username">Username</label>
+                  <input type="text" class="form-control">
+
+                  <label for="password">Password</label>
+                  <input type="text" id="password" name="password" class="form-control">
+
+
+                  <input type="submit" value="Sign In">
+                </form>
+              </div>
+            </div>
+          </div>
+    
+     
+   
+
+          <div class="col-lg-6">
+            <div class="form-container" style=" margin-left:100px;height: 400px;width: 400px; background: #fff; padding: 20px;border-radius: 5px; box-shadow: 0px 0px 10px 0px rgba(0,0,0,0.1);">
+              <div class="card-body">
+        <h2 class="row" style="font-size: 40px;">
+          <center>BloodBank</center>
+        </h2>
+        <form>
+          <label for="username">Username</label>
+          <input type="text" class="form-control">
+
+          <label for="password">Password</label>
+          <input type="text" id="password" name="password" class="form-control">
+
+
+          <input type="submit" value="Sign In">
+        </form>
+      </div>
+      </div>
+    </div>
+  </div>
+</body>
+
 </html>
